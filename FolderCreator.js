32c4683a07d8598982dfe81447ac4a1ea6d64155
EmilySeville7cfg <EmilySeville7cf@gmail.com>
2021-10-05 16:10:28 +1000

    FolderCreator rewritten via ES2015 classes

--- FolderCreator.js
+++ FolderCreator.js
@@ -2,52 +2,57 @@
 
 /**
  * Folder creator to add missing folders according to .json config.
- * @constructor
- * 
- * @param path {string} Config name with extension.
  */
-function FolderCreator(path, logger) {
-  this.__logger = logger === undefined ? new ConsoleLogger() : logger;
-
-  let validator = new Validator(path, logger);
-  if (!validator.IsValid())
-    throw new Error(`Config is incorrect. Possible reasons:
-- key value is not "*" or object
-- https://assertible.com/json endpoint is unavailable
-- internal error`);
-
+class FolderCreator {
   /**
-   * .json config content.
+   * Creates folder creator to add missing folders according to .json config.
+   * 
+   * @param {string} path Config name with extension.
+   * @param {string} [logger=ConsoleLogger] Logger to log all info.
    */
-  this.config = validator.config;
-}
+  constructor (path, logger = new ConsoleLogger()) {
+    this.__logger = logger;
 
-/**
- * Creates missing folders according to .json config.
- */
-FolderCreator.prototype.CreateMissingFolders = function () {
-  function create(where, parsed) {
-    for (const key in parsed) {
-      let iterator = where.getFoldersByName(key);
-      if (!iterator.hasNext()) {
-        this.__logger.writeInfo(`"${key}" folder created in "${where.getName()}".`);
-        where.createFolder(key);
-      }
-      else if (!iterator.next().isTrashed())
-        this.__logger.writeWarn(`Multiple "${key}" folders found in "${where.getName()}", "${key}" folder hasn't created in "${where.getName()}". Script execution results may be unpredictable.`);
-    }
+    let validator = new Validator(path, logger);
+    if (!validator.IsValid())
+      throw new Error(`Config is incorrect. Possible reasons:
+  - key value is not "*" or object
+  - https://assertible.com/json endpoint is unavailable
+  - internal error`);
 
-    for (const key in parsed) {
-      const value = parsed[key];
-      if (typeof value === "object") {
-        let iterator = where.getFoldersByName(key);
-        create.call(this, iterator.next(), value);
-      }
-    }
+    /**
+     * .json config content.
+     */
+    this.config = validator.config;
   }
 
-  const root = DriveApp.getRootFolder();
-  return create.call(this, root, JSON.parse(this.config))
+  /**
+   * Creates missing folders according to .json config.
+   */
+  CreateMissingFolders() {
+    function create(where, parsed) {
+      for (const key in parsed) {
+        let iterator = where.getFoldersByName(key);
+        if (!iterator.hasNext()) {
+          this.__logger.writeInfo(`"${key}" folder created in "${where.getName()}".`);
+          where.createFolder(key);
+        }
+        else if (!iterator.next().isTrashed())
+          this.__logger.writeWarn(`Multiple "${key}" folders found in "${where.getName()}", "${key}" folder hasn't created in "${where.getName()}". Script execution results may be unpredictable.`);
+      }
+
+      for (const key in parsed) {
+        const value = parsed[key];
+        if (typeof value === "object") {
+          let iterator = where.getFoldersByName(key);
+          create.call(this, iterator.next(), value);
+        }
+      }
+    }
+
+    const root = DriveApp.getRootFolder();
+    return create.call(this, root, JSON.parse(this.config))
+  }
 }
 
 Object.freeze(FolderCreator);
