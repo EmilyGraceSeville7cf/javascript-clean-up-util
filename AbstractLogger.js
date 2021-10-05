a1cca8244352e2b0068f6e2b2bf927ceee8a522a
EmilySeville7cfg <EmilySeville7cf@gmail.com>
2021-10-05 18:09:54 +1000

    AbstractLogger rewritten via ES2015 classes

--- AbstractLogger.js
+++ AbstractLogger.js
@@ -2,47 +2,40 @@
 
 /**
  * Abstract logger to write logs.
- * @constructor
  */
-function AbstractLogger() {
-}
+class AbstractLogger {
+  /**
+   * Writes normal message into log stream.
+   *
+   * @param {message} message Message to log.
+   */
+  writeLog(message) {}
 
-/**
- * Writes normal message into log stream.
- * 
- * @param path {message} Message to log.
- */
-AbstractLogger.prototype.writeLog = function (message) {
-}
+  /**
+   * Writes info message into log stream.
+   *
+   * @param {message} message Message to log.
+   */
+  writeInfo(message) {}
 
-/**
- * Writes info message into log stream.
- * 
- * @param path {message} Message to log.
- */
-AbstractLogger.prototype.writeInfo = function (message) {
-}
+  /**
+   * Writes warn message into log stream.
+   *
+   * @param {message} message Message to log.
+   */
+  writeWarn(message) {}
 
-/**
- * Writes warn message into log stream.
- * 
- * @param path {message} Message to log.
- */
-AbstractLogger.prototype.writeWarn = function (message) {
-}
+  /**
+   * Writes error message into log stream.
+   *
+   * @param {message} message Message to log.
+   */
+  writeError(message) {}
 
-/**
- * Writes error message into log stream.
- * 
- * @param path {message} Message to log.
- */
-AbstractLogger.prototype.writeError = function (message) {
-}
-
-/**
- * Writes empty line into log stream.
- */
-AbstractLogger.prototype.writeLine = function () {
+  /**
+   * Writes empty line into log stream.
+   */
+  writeLine() {}
 }
 
 Object.freeze(AbstractLogger);
