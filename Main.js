bdef615c8e3215e2df4f47c987a5b7b1eb6f5505
EmilySeville7cfg <EmilySeville7cf@gmail.com>
2021-10-06 11:39:17 +1000

    Code fixed according to Google style guide

--- Main.js
+++ Main.js
@@ -1,23 +1,17 @@
-/*
-  Sample clean-up-config.json: https://drive.google.com/file/d/1QTkxwH2A4tzMW3zHk4s-IKSpcwmTZ9Pi/view?usp=sharing.
-
-  - Every config key is folder name to be created when there is no another folder in the current directory.
-  - Every config value is "*" when there is no need to traverse folders any more.
-  - Every config value is object that follows the same rules when it is needed to traverse folders down.
-*/
-
-"use strict";
+/* eslint-disable require-jsdoc */
+/* eslint-disable no-unused-vars */
+'use strict';
 
 function main() {
-  let logger = customloggerslib.newTextLogger("0AJJhQCu2pGhAUk9PVA", "clean-up-log.log");
+  const logger = customloggerslib.newTextLogger('0AJJhQCu2pGhAUk9PVA',
+      'clean-up-log.log');
 
   try {
-    logger.writeInfo("Checking config...");
-    let creator = new FolderCreator("clean-up-config.json", logger);
-    logger.writeInfo("Config is correct.");
-    creator.CreateMissingFolders();
-  }
-  catch (e) {
+    logger.writeInfo('Checking config...');
+    const creator = new FolderCreator('clean-up-config.json', logger);
+    logger.writeInfo('Config is correct.');
+    creator.createMissingFolders();
+  } catch (e) {
     logger.writeError(`${e.message}\n`);
   }
   logger.writeLine();
